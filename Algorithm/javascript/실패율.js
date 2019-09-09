9a0f92a60ae564ff214f87a181a36698e53744e7
Sub2n <95su1208@gmail.com>
2019-09-09 23:50:08 +0900

    실패율

--- Algorithm/javascript/실패율.js
+++ Algorithm/javascript/실패율.js
@@ -3,41 +3,48 @@ function solution(N, stages) {
   let failure = [];
   let player = 0;
 
-  stages.sort().forEach((stage) => {
-    let fail = {};
-
-    fail[stage] = !failure[stage - 1] ? 1 : failure[stage - 1][stage] + 1;
-
-    failure[stage - 1] = fail;
-  })
-
-  for (let i = 0; i < failure.length; i++) {
-    if (!failure[i]) {
-      let fail = {};
-      fail[i + 1] = 0;
-      failure[i] = fail;
-    }
-    player += failure[i][i + 1];
+  for (let i = 0; i < N; i++) {
+    failure[i] = 0;
   }
+
+  console.log(failure);
+
+  stages.forEach(stage => {});
+
+  // stages.sort().forEach(stage => {
+  //   let fail = {};
+
+  //   fail[stage] = !failure[stage - 1] ? 1 : failure[stage - 1][stage] + 1;
+
+  //   failure[stage - 1] = fail;
+  // });
+
+  // for (let i = 0; i < failure.length; i++) {
+  //   if (!failure[i]) {
+  //     let fail = {};
+  //     fail[i + 1] = 0;
+  //     failure[i] = fail;
+  //   }
+  //   player += failure[i][i + 1];
+  // }
   // console.dir(failure);
 
   failure.reduce((acc, cur, idx) => {
     // console.log(idx, acc, cur, player - acc);
     let fr = {
       stage: idx + 1,
-      ratio: cur[idx + 1] / (player - acc)
+      ratio: [cur[idx + 1], player - acc],
     };
     if (fr.stage <= N) answer.push(fr);
     return acc + cur[idx + 1];
   }, 0);
 
-  // console.log(answer);
-
-  return answer.sort((prev, cur) => {
-    return prev.ratio < cur.ratio;
-  }).map(({
-    stage
-  }) => stage)
+  return answer
+    .sort((prev, cur) => {
+      return prev.ratio[0] * cur.ratio[1] < prev.ratio[1] * cur.ratio[0];
+      // return prev.ratio < cur.ratio;
+    })
+    .map(({ stage }) => stage);
 }
 
 console.log('test1', solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
