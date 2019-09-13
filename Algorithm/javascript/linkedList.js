38aa28cf8a2e77892df4be0dffd69341b16ec41e
Sub2n <95su1208@gmail.com>
2019-09-13 23:02:46 +0900

    linkedlist

--- Algorithm/javascript/linkedList.js
+++ Algorithm/javascript/linkedList.js
@@ -1,17 +1,3 @@
-// class ThreeLetterChain {
-// public:
-// 	//리스트 조작 연산
-// 	.
-// 	.
-// private:
-// 	class ThreeLetterNode {   //중첩 클래스
-// 	public:
-// 		char data[3];
-// 		ThreeLetterNode *link;
-// 	};
-// 	ThreeLetterNode *first;
-// };
-
 class ListNode {
   constructor(data) {
     this.data = data;
