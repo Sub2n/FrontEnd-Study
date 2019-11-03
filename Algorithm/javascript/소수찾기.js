a95f812a03a25d8cf31ff924c295b80466ab6efa
Sub2n <95su1208@gmail.com>
2019-11-03 15:39:35 +0900

    permutation(순열) 이용한 완전탐색

---
+++ Algorithm/javascript/소수찾기.js
@@ -0,0 +1,48 @@
+function isPrime(n) {
+  if (n <= 1) return false;
+
+  for (let i = 2; i < n; ++i) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+function swap(arr, n1, n2) {
+  const temp = arr[n1];
+  arr[n1] = arr[n2];
+  arr[n2] = temp;
+}
+
+function makeNumbers(set, arr, index, toPick) {
+  if (index === toPick) {
+    set.add(+arr.filter((org, i) => i < toPick).join(''));
+    console.log(set)
+  } else {
+    for (let i = index; i < arr.length; ++i) {
+      swap(arr, index, i);
+      makeNumbers(set, arr, index + 1, toPick);
+      swap(arr, index, i);
+    }
+  }
+}
+
+function solution(numbers) {
+  const nums = new Set();
+  const arr = numbers.split('');
+
+  for (let toPick = 1; toPick <= arr.length; ++toPick) {
+    makeNumbers(nums, arr, 0, toPick);
+  }
+
+  let ans = 0;
+
+  nums.forEach((num) => {
+    if (isPrime(+num)) ans += 1;
+  })
+
+  return ans;
+}
+
+// console.log(solution("17") === 3);
+// console.log(solution("011") === 2);
