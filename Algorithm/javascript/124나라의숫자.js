025eb2b6103b36f32c90c94e011a5bcd7d3a976c
Sub2n <95su1208@gmail.com>
2019-11-07 22:34:18 +0900

    [프로그래머스] 124나라의 숫자 - 3진법 응용

---
+++ Algorithm/javascript/124나라의숫자.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  let ans = '';
+  while (n > 0) {
+    if (n % 3 === 0) {
+      ans = '4' + ans;
+      n = Math.floor(n / 3) - 1;
+    } else {
+      ans = (n % 3) + ans;
+      n = Math.floor(n / 3);
+    }
+  }
+  console.log(ans)
+  return ans;
+}
+
+console.log(solution(10) === '41');
