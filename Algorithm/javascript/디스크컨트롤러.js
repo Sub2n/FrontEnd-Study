009ddd081e34081131d04af33c578f3d64f8f955
Sub2n <95su1208@gmail.com>
2019-12-19 21:56:34 +0900

    [프로그래머스] 디스크 컨트롤러 - Heap. Proirity Queue

--- Algorithm/javascript/디스크컨트롤러.js
+++ Algorithm/javascript/디스크컨트롤러.js
@@ -1,6 +1,30 @@
 /*
-throughput 고려 
+ 작업 소요시간 작은 순서대로 우선순위 큐
 */
 function solution(jobs) {
+  jobs = jobs.sort((a, b) => a[1] - b[1]);
+  const length = jobs.length;
+  let throughputs = 0;
+  let time = 0;
 
+  while (jobs.length > 0) {
+    for (let i = 0; i < jobs.length; i++) {
+      const job = jobs[i];
+      if (time >= job[0]) {
+        throughputs += time - job[0] + job[1];
+        time += job[1];
+        jobs = jobs.filter((e, idx) => idx !== i);
+        break;
+      }
+      if (i === jobs.length - 1) time += 1;
+    }
+  }
+  return Math.floor(throughputs / length);
 }
+
+console.log(solution([
+  [0, 3],
+  [1, 9],
+  [3, 6],
+  [2, 15]
+]) === 9);
