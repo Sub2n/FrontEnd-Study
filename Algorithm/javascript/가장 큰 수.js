a3710662a89ad43f5052672cf7af61912a1c8491
Sub2n <95su1208@gmail.com>
2019-12-25 23:03:10 +0900

    푸는중

---
+++ Algorithm/javascript/가장 큰 수.js
@@ -0,0 +1,23 @@
+/*
+0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+
+0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+정답이 클 수 있으니 문자열로 바꾸어 리턴
+*/
+
+// 재귀이용?
+function makeNumbers(numbers) {
+  if (numbers.length === 0) return;
+}
+
+function solution(numbers) {
+  var answer = '';
+  return answer;
+}
+
+console.log(solution([6, 10, 2])); // 6210
+console.log(solution([
+  [3, 30, 34, 5, 9]
+])); // 9534330
