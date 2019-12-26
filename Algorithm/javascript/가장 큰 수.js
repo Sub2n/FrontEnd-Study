4b9ad0ae0dccd46d87924907a61c373ebfb5fc2a
Sub2n <95su1208@gmail.com>
2019-12-26 23:00:19 +0900

    완전탐색 적용 (재귀)

--- Algorithm/javascript/가장 큰 수.js
+++ Algorithm/javascript/가장 큰 수.js
@@ -7,17 +7,34 @@
 정답이 클 수 있으니 문자열로 바꾸어 리턴
 */
 
-// 재귀이용?
-function makeNumbers(numbers) {
-  if (numbers.length === 0) return;
+// 완전탐색
+function swap(arr, n1, n2) {
+  const temp = arr[n1];
+  arr[n1] = arr[n2];
+  arr[n2] = temp;
+}
+
+function makeNumbers(set, arr, index, toPick) {
+  if (index === toPick) {
+    set.add(+arr.filter((org, i) => i < toPick).join(''));
+  } else {
+    for (let i = index; i < arr.length; ++i) {
+      swap(arr, index, i);
+      makeNumbers(set, arr, index + 1, toPick);
+      swap(arr, index, i);
+    }
+  }
 }
 
 function solution(numbers) {
-  var answer = '';
-  return answer;
+  const nums = new Set();
+
+  makeNumbers(nums, numbers, 0, numbers.length);
+
+  console.log(Array.from(nums).sort((a, b) => b - a))
+
+  return Array.from(nums).sort((a, b) => b - a)[0] + '';
 }
 
 console.log(solution([6, 10, 2])); // 6210
-console.log(solution([
-  [3, 30, 34, 5, 9]
-])); // 9534330
+console.log(solution([3, 30, 34, 5, 9])); // 9534330
