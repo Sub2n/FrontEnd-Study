5c2c962986b95898fdfce9768181853f770b6e39
Sub2n <95su1208@gmail.com>
2019-12-29 23:57:22 +0900

    [프로그래머스] 탑 - 스택/큐

--- Algorithm/javascript/탑.js
+++ Algorithm/javascript/탑.js
@@ -1,19 +1,26 @@
 // 발사한 신호는 신호를 보낸 탑보다 높은 탑에서만 수신합니다. 또한, 한 번 수신된 신호는 다른 탑으로 송신되지 않습니다.
 function solution(heights) {
   const answer = [];
-  let check = false;
+  let check;
+
   while (heights.length > 0) {
     const top = heights.pop();
-    for (let i = heights.length - 1; i > 0; i--) {
-      console.log(heights, i, top, answer)
+    check = false;
+
+    for (let i = heights.length - 1; i >= 0; i--) {
       if (heights[i] > top) {
         answer.unshift(i + 1);
+        check = true;
         break;
       }
     }
-    answer.unshift(0);
+
+    if (!check) answer.unshift(0);
   }
+
   return answer;
 }
 
-console.log(solution([6, 9, 5, 7, 4])); // [0,0,2,2,4]
+// console.log(solution([6, 9, 5, 7, 4])); // [0,0,2,2,4]
+// console.log(solution([9, 1, 1, 1])); //[0, 1, 1, 1]
+console.log(solution([7, 6, 5, 4, 3, 2, 1])); // 	[0, 1, 2, 3, 4, 5, 6]
