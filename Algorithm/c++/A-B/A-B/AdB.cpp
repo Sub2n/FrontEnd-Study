a6ceeb0b6225f60e0852036fbbf9085c30f705f5
Sub2n <95su1208@gmail.com>
2020-01-02 15:35:04 +0900

    2. JSX

--- Algorithm/c++/A-B/A-B/AdB.cpp
+++ Algorithm/c++/A-B/A-B/AdB.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
 using namespace std;
 
 int main() {
 	int a, b;
-	scanf_s("%d %d", &a, &b);
-	double r = a / b;
-	printf("%f\n", r);
-	for (int i = 0; i < 10; i++) {
-		printf("%f\n", r);
-		r += (a % b) / 10 ^ i;
+	//scanf_s("%d %d", &a, &b);
+	cin >> a >> b;
+	string s = "";
+	s += to_string(a / b);
+	if (a % b != 0) s += ".";
+	for (int i = 0; i < 9; i++) {
+		s += to_string((a * 10) / b);
 	}
-	printf("%f\n", r);
+	cout << s << endl;
 }
--- React/repos/hello-react/src/App.js
+++ React/repos/hello-react/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 주석은 이렇게 작성합니다. */}
+      <div
+        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있습니다.
+      >
+        {name}
+      </div>
+      <input />
+    </>
   );
 }
 
