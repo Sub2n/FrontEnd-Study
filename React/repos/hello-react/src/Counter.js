6ac5a25ee014afd163a247b72455f997002f320a
Sub2n <95su1208@gmail.com>
2020-01-03 12:01:33 +0900

    3.4. state

--- React/repos/hello-react/src/Counter.js
+++ React/repos/hello-react/src/Counter.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 }, () => {
+              alert('origin updated!');
+            });
+            this.setState(
+              prevState => ({
+                number: prevState.number + 1
+              }),
+              () => {
+                alert('functional updated!');
+              }
+            );
           }}>
           +1
         </button>
---
+++ React/repos/hello-react/src/Say.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+  const [message, setMessage] = useState('');
+  const [color, setColor] = useState('black');
+  const onClickEnter = () => setMessage('안녕하세요!');
+  const onClickLeave = () => setMessage('안녕히 가세요!');
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        빨간색
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        초록색
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        파란색
+      </button>
+    </div>
+  );
+};
+
+export default Say;
