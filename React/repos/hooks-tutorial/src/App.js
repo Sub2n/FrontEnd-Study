795666410f5b55eaa2138804bd315bf99f07cfde
Sub2n <95su1208@gmail.com>
2020-01-05 13:44:46 +0900

    8.3. useReducer

--- React/repos/hooks-tutorial/src/App.js
+++ React/repos/hooks-tutorial/src/App.js
@@ -1,18 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Info from './Info';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-
-  return (
-    <div>
-      <button onClick={() => setVisible(!visible)}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 };
 
 export default App;
