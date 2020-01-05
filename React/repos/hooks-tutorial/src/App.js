79f89f653a2c95cd41875908e9697d84f65f5d39
Sub2n <95su1208@gmail.com>
2020-01-05 13:20:32 +0900

    8.2. useEffect

--- React/repos/hooks-tutorial/src/App.js
+++ React/repos/hooks-tutorial/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Info from './Info';
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <div>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
--- React/repos/hooks-tutorial/src/Info.js
+++ React/repos/hooks-tutorial/src/Info.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    return () => {
+      console.log('cleanup');
+    };
+  }, []);
 
   const onChangeName = e => setName(e.target.value);
   const onChangeNickname = e => setNickname(e.target.value);
