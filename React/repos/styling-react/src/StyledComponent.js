6453c1d02f672e43dc612ce4bbf30581d0fdb913
Sub2n <95su1208@gmail.com>
2020-01-06 10:38:38 +0900

    9. 컴포넌트 스타일링

--- React/repos/styling-react/src/StyledComponent.js
+++ React/repos/styling-react/src/StyledComponent.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768
+};
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}rem) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+});
+
 const Box = styled.div`
   background: ${props => props.color || 'blue'};
   padding: 1rem;
   display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`}
 `;
 
 const Button = styled.button`
