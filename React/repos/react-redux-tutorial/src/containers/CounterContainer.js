dcd593fd603e81704aed9aa68e4586a620cb1df4
Sub2n <95su1208@gmail.com>
2020-01-14 13:40:13 +0900

    17. 리덕스를 사용해 리액트 애플리케이션 상태 관리하기

--- React/repos/react-redux-tutorial/src/containers/CounterContainer.js
+++ React/repos/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-export default connect(
-  state => ({
-    number: state.counter.number
-  }),
-  {
-    increase,
-    decrease
-  }
-)(CounterContainer);
+export default React.memo(CounterContainer);
--- React/repos/react-redux-tutorial/src/containers/TodosContainer.js
+++ React/repos/react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import Todos from '../components/Todos';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
+import useActions from '../lib/useActions';
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({ input: todos.input, todos: todos.todos }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove
-  }
-)(TodosContainer);
+export default React.memo(TodosContainer);
